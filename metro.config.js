6ba73031dd3f09deeb07e5fdc827a4d324d8ac11
Afrie Irham <afrieirham.work@gmail.com>
2022-11-16 05:13:36 +0800

    build: fix broken svg imports

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require("expo/metro-config")
 
-const config = getDefaultConfig(__dirname)
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname)
 
-const getResolver = async () => {
-  const {
-    resolver: {
-      sourceExts,
-      assetExts
-    }
-  } = await getDefaultConfig(__dirname)
+  const { transformer, resolver } = config
 
-  return {
-    assetExts: assetExts.filter(ext => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   }
-}
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  }
 
-config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer")
-config.resolver = getResolver()
-
-
-
-module.exports = config;
+  return config
+})();
 
